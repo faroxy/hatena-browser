92796b205b213d14cc17e8c2830ed3380940ec97
nakagawakeita <t05m047b@yahoo.co.jp>
2016-08-04 17:37:59 +0900

    first

---
+++ hatena-browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hatena-browser
+//
+//  Created by nakagawakeita on 2016/08/04.
+//  Copyright © 2016年 continue.Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
